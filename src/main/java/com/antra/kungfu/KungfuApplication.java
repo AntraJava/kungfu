51a4151ef427cd6fda0f36eb4fb6e76c5f06cf66
antrajava <zhuangdawei@gmail.com>
2018-05-11 21:16:45 -0400

    Initial commit

---
+++ src/main/java/com/antra/kungfu/KungfuApplication.java
@@ -0,0 +1,12 @@
+package com.antra.kungfu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KungfuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KungfuApplication.class, args);
+	}
+}
