7b4ab11a7d9f2f00c9ee799f5bc453a2966efc05
Dawei Zhuang <dawei.zhuang@antra.net>
2018-05-14 13:56:15 -0400

    add freedom

--- src/main/java/com/antra/kungfu/FirstController.java
+++ src/main/java/com/antra/kungfu/FirstController.java
@@ -1,7 +1,14 @@
 package com.antra.kungfu;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
+    @GetMapping("/")
+    public ResponseEntity<Object> getFirst() {
+        return new ResponseEntity<Object>("Freedom", HttpStatus.OK);
+    }
 }
